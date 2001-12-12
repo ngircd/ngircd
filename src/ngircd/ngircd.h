0f6d3bdea35d69ba061cb08077c38909eab874c7
Alexander Barton <alex@barton.de>
2001-12-12 23:30:01 +0000

    - NGIRCd_Quit ist nun das globale Flag zum Beenden des ngircd.

--- src/ngircd/ngircd.h
+++ src/ngircd/ngircd.h
@@ -9,19 +9,26 @@
  * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
  * der an comBase beteiligten Autoren finden Sie in der Datei AUTHORS.
  *
- * $Id: ngircd.h,v 1.1 2001/12/11 21:53:04 alex Exp $
+ * $Id: ngircd.h,v 1.2 2001/12/12 23:30:01 alex Exp $
  *
  * ngircd.h: Prototypen aus dem "Haupt-Modul"
  *
  * $Log: ngircd.h,v $
- * Revision 1.1  2001/12/11 21:53:04  alex
- * Initial revision
+ * Revision 1.2  2001/12/12 23:30:01  alex
+ * - NGIRCd_Quit ist nun das globale Flag zum Beenden des ngircd.
+ *
+ * Revision 1.1.1.1  2001/12/11 21:53:04  alex
+ * Imported sources to CVS.
  *
  */
 
 
 #ifndef __ngircd_h__
 #define __ngircd_h__
+
+
+GLOBAL BOOLEAN NGIRCd_Quit;		/* TRUE: Hauptschleife beenden */
+
 
 #endif
 

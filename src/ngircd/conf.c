cbc1e59fb23dd1f2a92bba4188a7ffe87360f046
Alexander Barton <alex@barton.de>
2001-12-12 17:18:20 +0000

    - Modul fuer Server-Konfiguration begonnen.

---
+++ src/ngircd/conf.c
@@ -0,0 +1,45 @@
+/*
+ * ngIRCd -- The Next Generation IRC Daemon
+ * Copyright (c)2001 by Alexander Barton (alex@barton.de)
+ *
+ * Dieses Programm ist freie Software. Sie koennen es unter den Bedingungen
+ * der GNU General Public License (GPL), wie von der Free Software Foundation
+ * herausgegeben, weitergeben und/oder modifizieren, entweder unter Version 2
+ * der Lizenz oder (wenn Sie es wuenschen) jeder spaeteren Version.
+ * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
+ * der an comBase beteiligten Autoren finden Sie in der Datei AUTHORS.
+ *
+ * $Id: conf.c,v 1.1 2001/12/12 17:18:20 alex Exp $
+ *
+ * conf.h: Konfiguration des ngircd
+ *
+ * $Log: conf.c,v $
+ * Revision 1.1  2001/12/12 17:18:20  alex
+ * - Modul fuer Server-Konfiguration begonnen.
+ *
+ */
+
+
+#include <portab.h>
+#include "global.h"
+
+#include <imp.h>
+#include <assert.h>
+
+#include <exp.h>
+#include "conf.h"
+
+
+GLOBAL VOID Conf_Init( VOID )
+{
+	/* ... */
+} /* Config_Init */
+
+
+GLOBAL VOID Conf_Exit( VOID )
+{
+	/* ... */
+} /* Config_Exit */
+
+
+/* -eof- */
---
+++ src/ngircd/conf.h
@@ -0,0 +1,34 @@
+/*
+ * ngIRCd -- The Next Generation IRC Daemon
+ * Copyright (c)2001 by Alexander Barton (alex@barton.de)
+ *
+ * Dieses Programm ist freie Software. Sie koennen es unter den Bedingungen
+ * der GNU General Public License (GPL), wie von der Free Software Foundation
+ * herausgegeben, weitergeben und/oder modifizieren, entweder unter Version 2
+ * der Lizenz oder (wenn Sie es wuenschen) jeder spaeteren Version.
+ * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
+ * der an comBase beteiligten Autoren finden Sie in der Datei AUTHORS.
+ *
+ * $Id: conf.h,v 1.1 2001/12/12 17:18:20 alex Exp $
+ *
+ * conf.h: Konfiguration des ngircd (Header)
+ *
+ * $Log: conf.h,v $
+ * Revision 1.1  2001/12/12 17:18:20  alex
+ * - Modul fuer Server-Konfiguration begonnen.
+ *
+ */
+
+
+#ifndef __conf_h__
+#define __conf_h__
+
+
+GLOBAL VOID Conf_Init( VOID );
+GLOBAL VOID Conf_Exit( VOID );
+
+
+#endif
+
+
+/* -eof- */

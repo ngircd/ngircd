cdb247c6667b25d8e6c7a8f006deb2b617f4b9a7
Alexander Barton <alex@barton.de>
2001-12-12 01:58:17 +0000

    - fuer fehlende Dateien werden nun "nur noch" symbolische Links erzeugt.

--- autogen.sh
+++ autogen.sh
@@ -1,19 +1,22 @@
 #!/bin/sh
 #
-# $Id: autogen.sh,v 1.1 2001/12/11 21:53:04 alex Exp $
+# $Id: autogen.sh,v 1.2 2001/12/12 01:58:17 alex Exp $
 #
 # $Log: autogen.sh,v $
-# Revision 1.1  2001/12/11 21:53:04  alex
-# Initial revision
+# Revision 1.2  2001/12/12 01:58:17  alex
+# - fuer fehlende Dateien werden nun "nur noch" symbolische Links erzeugt.
+#
+# Revision 1.1.1.1  2001/12/11 21:53:04  alex
+# Imported sources to CVS.
 #
 
 if [ -f configure ]; then
-	echo "autogen.sh: configure-Skript existiert bereits!"
+	echo "autogen.sh: configure-Skript existiert bereits ..."
 fi
 
 aclocal && \
  autoheader && \
- automake --add-missing --copy && \
+ automake --add-missing && \
  autoconf && \
  echo "Okay, autogen.sh war erfolgreich."
 

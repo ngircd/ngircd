660ef2307abae647e7c582250f0bec94dbc80518
Alexander Barton <alex@barton.de>
2001-12-24 01:30:46 +0000

    - einige Messages korrigiert, andere ergaenzt (u.a. fuer MOTD).

--- src/ngircd/messages.h
+++ src/ngircd/messages.h
@@ -9,11 +9,14 @@
  * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
  * der an comBase beteiligten Autoren finden Sie in der Datei AUTHORS.
  *
- * $Id: messages.h,v 1.1 2001/12/23 21:53:32 alex Exp $
+ * $Id: messages.h,v 1.2 2001/12/24 01:30:46 alex Exp $
  *
  * irc.h: IRC-Befehle (Header)
  *
  * $Log: messages.h,v $
+ * Revision 1.2  2001/12/24 01:30:46  alex
+ * - einige Messages korrigiert, andere ergaenzt (u.a. fuer MOTD).
+ *
  * Revision 1.1  2001/12/23 21:53:32  alex
  * - Ich habe diesen Header begonnen.
  *
@@ -27,16 +30,25 @@
 
 
 #define RPL_WELCOME			"001"
-#define RPL_WELCOME_MSG			RPL_WELCOME" :Welcome to the Internet Relay Network %s!%s@%s"
+#define RPL_WELCOME_MSG			RPL_WELCOME" %s :Welcome to the Internet Relay Network %s!%s@%s"
 
 #define RPL_YOURHOST			"002"
-#define RPL_YOURHOST_MSG		RPL_YOURHOST" :Your host is %s, running ngircd "VERSION
+#define RPL_YOURHOST_MSG		RPL_YOURHOST" %s :Your host is %s, running ngircd "VERSION
 
 #define RPL_CREATED			"003"
-#define RPL_CREATED_MSG			RPL_CREATED" :This server was created once upon a time ... ;-)"
+#define RPL_CREATED_MSG			RPL_CREATED" %s :This server was created once upon a time ... ;-)"
 
 #define RPL_MYINFO			"004"
-#define RPL_MYINFO_MSG			RPL_MYINFO" :ngircd "VERSION" + +"
+#define RPL_MYINFO_MSG			RPL_MYINFO" %s %s ngircd-"VERSION" + +"
+
+#define RPL_MOTDSTART			"375"
+#define RPL_MOTDSTART_MSG		RPL_MOTDSTART" %s :- %s Message of the day - "
+
+#define RPL_MOTD			"372"
+#define RPL_MOTD_MSG			RPL_MOTD" %s :- %s"
+
+#define RPL_ENDOFMOTD			"376"
+#define RPL_ENDOFMOTD_MSG		RPL_ENDOFMOTD" %s :End of MOTD command."
 
 
 #define ERR_UNKNOWNCOMMAND		"421"

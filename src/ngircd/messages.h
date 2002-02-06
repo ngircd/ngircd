5c48cbff1d3b8dfd102ca77416a746f42f3dd2f6
Alexander Barton <alex@barton.de>
2002-02-06 16:51:39 +0000

    - neue Text-Konstante RPL_UMODEISCHAN_MSG.

--- src/ngircd/messages.h
+++ src/ngircd/messages.h
@@ -9,11 +9,14 @@
  * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
  * der an ngIRCd beteiligten Autoren finden Sie in der Datei AUTHORS.
  *
- * $Id: messages.h,v 1.23 2002/01/29 00:14:05 alex Exp $
+ * $Id: messages.h,v 1.24 2002/02/06 16:51:39 alex Exp $
  *
  * irc.h: IRC-Befehle (Header)
  *
  * $Log: messages.h,v $
+ * Revision 1.24  2002/02/06 16:51:39  alex
+ * - neue Text-Konstante RPL_UMODEISCHAN_MSG.
+ *
  * Revision 1.23  2002/01/29 00:14:05  alex
  * - RPL_WHOISCHANNELS_MSG korrigiert.
  *
@@ -133,6 +136,7 @@
 
 #define RPL_UMODEIS			"211"
 #define RPL_UMODEIS_MSG			RPL_UMODEIS" %s +%s"
+#define RPL_UMODEISCHAN_MSG		RPL_UMODEIS" %s %s +%s"
 
 #define RPL_USERHOST			"302"
 #define RPL_USERHOST_MSG		RPL_USERHOST" %s :"

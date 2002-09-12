05bf66dff0cdceeec9e9da317f0b0a90fa135313
Alexander Barton <alex@barton.de>
2002-09-12 02:27:30 +0000

    - Test-Server wird in aktuelles Verzeichnis als "ngircd-TEST" verlinkt. - die PID des Test-ngIRCd wird besser ermittelt.

--- src/testsuite/start-server.sh
+++ src/testsuite/start-server.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: start-server.sh,v 1.2 2002/09/09 22:56:07 alex Exp $
+# $Id: start-server.sh,v 1.3 2002/09/12 02:27:30 alex Exp $
 
 echo "      starting server ..."
 
+rm -rf logs
+
 echo "This is an ngIRCd Test Server" > ngircd-test.motd
 
-../ngircd/ngircd -np -f ngircd-test.conf > ngircd-test.log 2>&1 &
+./ngircd-TEST -np -f ${srcdir}/ngircd-test.conf > ngircd-test.log 2>&1 &
 sleep 1
 
-pid=`ps a | grep ngircd-test | head -n 1 | cut -d ' ' -f 1`
+ps a > procs.tmp
+pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$1 }"`
 kill -0 $pid > /dev/null 2>&1
-
-rm -rf logs
 
 # -eof-

3f873b0c39cfc466ae44602f4cec39dae98c6d8b
Alexander Barton <alex@barton.de>
2002-09-12 02:27:47 +0000

    - PID des Test-Servers wird besser ermittelt.

--- src/testsuite/stop-server.sh
+++ src/testsuite/stop-server.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: stop-server.sh,v 1.1 2002/09/09 10:16:24 alex Exp $
+# $Id: stop-server.sh,v 1.2 2002/09/12 02:27:47 alex Exp $
 
 echo "      stopping server ..."
 
-pid=`ps a | grep ngircd-test | head -n 1 | cut -d ' ' -f 1`
+ps a > procs.tmp
+pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$1 }"`
 kill $pid > /dev/null 2>&1
 
 # -eof-

508fa48aa6342beb50e510285b683abe54e0fe92
Alexander Barton <alex@barton.de>
2002-09-16 09:53:16 +0000

    - es wird nun getestet, mit welchen Parametern "ps" aufgerufen werden muss.

--- src/testsuite/start-server.sh
+++ src/testsuite/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: start-server.sh,v 1.4 2002/09/13 06:04:49 alex Exp $
+# $Id: start-server.sh,v 1.5 2002/09/16 09:53:16 alex Exp $
 
 echo "      starting server ..."
 
@@ -11,8 +11,12 @@ echo "This is an ngIRCd Test Server" > ngircd-test.motd
 ./ngircd-TEST -np -f ${srcdir}/ngircd-test.conf > ngircd-test.log 2>&1 &
 sleep 1
 
-ps ax > procs.tmp
-pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$1 }"`
+PS_FLAGS=a; PS_PIDCOL=1
+ps a > /dev/null 2>&1
+if [ $? -ne 0 ]; then PS_FLAGS=-f; PS_PIDCOL=2; fi
+
+ps $PS_FLAGS > procs.tmp
+pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$$PS_PIDCOL }"`
 kill -0 $pid > /dev/null 2>&1
 
 # -eof-
--- src/testsuite/stop-server.sh
+++ src/testsuite/stop-server.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: stop-server.sh,v 1.3 2002/09/13 06:04:49 alex Exp $
+# $Id: stop-server.sh,v 1.4 2002/09/16 09:53:16 alex Exp $
 
 echo "      stopping server ..."
 
-ps ax > procs.tmp
-pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$1 }"`
+PS_FLAGS=a; PS_PIDCOL=1
+ps a > /dev/null 2>&1
+if [ $? -ne 0 ]; then PS_FLAGS=-f; PS_PIDCOL=2; fi
+
+ps $PS_FLAGS > procs.tmp
+pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$$PS_PIDCOL }"`
 kill $pid > /dev/null 2>&1
 
 # -eof-

b5d6597c0ab0c7aa2ada7a9083cbc49f5c92e286
Alexander Barton <alex@barton.de>
2002-09-18 20:58:56 +0000

    - Parameter fuer ps werden in anderer (besserer) Reihenfolge getestet.

--- src/testsuite/stop-server.sh
+++ src/testsuite/stop-server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: stop-server.sh,v 1.4 2002/09/16 09:53:16 alex Exp $
+# $Id: stop-server.sh,v 1.5 2002/09/18 20:58:56 alex Exp $
 
 echo "      stopping server ..."
 
-PS_FLAGS=a; PS_PIDCOL=1
-ps a > /dev/null 2>&1
-if [ $? -ne 0 ]; then PS_FLAGS=-f; PS_PIDCOL=2; fi
+PS_FLAGS=-f; PS_PIDCOL=2
+ps $PS_FLAGS > /dev/null 2>&1
+if [ $? -ne 0 ]; then PS_FLAGS=a; PS_PIDCOL=1; fi
 
 ps $PS_FLAGS > procs.tmp
 pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$$PS_PIDCOL }"`

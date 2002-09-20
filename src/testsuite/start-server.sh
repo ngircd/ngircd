cf20b16d2aa3926d598c61e919083264cac1c12c
Alexander Barton <alex@barton.de>
2002-09-20 13:46:22 +0000

    - Flags bei "ps" werden in anderer Reihenfolge geprueft (aus CVS-HEAD).

--- src/testsuite/start-server.sh
+++ src/testsuite/start-server.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # ngIRCd Test Suite
-# $Id: start-server.sh,v 1.5 2002/09/16 09:53:16 alex Exp $
+# $Id: start-server.sh,v 1.5.2.1 2002/09/20 13:46:22 alex Exp $
 
 echo "      starting server ..."
 
@@ -11,9 +11,9 @@ echo "This is an ngIRCd Test Server" > ngircd-test.motd
 ./ngircd-TEST -np -f ${srcdir}/ngircd-test.conf > ngircd-test.log 2>&1 &
 sleep 1
 
-PS_FLAGS=a; PS_PIDCOL=1
-ps a > /dev/null 2>&1
-if [ $? -ne 0 ]; then PS_FLAGS=-f; PS_PIDCOL=2; fi
+PS_FLAGS=-f; PS_PIDCOL=2
+ps $PS_FLAGS > /dev/null 2>&1
+if [ $? -ne 0 ]; then PS_FLAGS=a; PS_PIDCOL=1; fi
 
 ps $PS_FLAGS > procs.tmp
 pid=`cat procs.tmp | grep ngircd-TEST | awk "{ print \\\$$PS_PIDCOL }"`

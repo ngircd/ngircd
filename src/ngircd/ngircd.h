fa7b3f0e4cbefdd51a499beabc4092bd3515ef42
Alexander Barton <alex@barton.de>
2002-12-12 11:30:23 +0000

    - new file header format.

--- src/ngircd/ngircd.h
+++ src/ngircd/ngircd.h
@@ -2,16 +2,15 @@
  * ngIRCd -- The Next Generation IRC Daemon
  * Copyright (c)2001,2002 by Alexander Barton (alex@barton.de)
  *
- * Dieses Programm ist freie Software. Sie koennen es unter den Bedingungen
- * der GNU General Public License (GPL), wie von der Free Software Foundation
- * herausgegeben, weitergeben und/oder modifizieren, entweder unter Version 2
- * der Lizenz oder (wenn Sie es wuenschen) jeder spaeteren Version.
- * Naehere Informationen entnehmen Sie bitter der Datei COPYING. Eine Liste
- * der an ngIRCd beteiligten Autoren finden Sie in der Datei AUTHORS.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Please read the file COPYING, README and AUTHORS for more information.
  *
- * $Id: ngircd.h,v 1.16 2002/11/22 23:31:23 alex Exp $
+ * $Id: ngircd.h,v 1.17 2002/12/12 11:30:23 alex Exp $
  *
- * ngircd.h: Prototypen aus dem "Haupt-Modul"
+ * Prototypes of the "main module".
  */
 
 

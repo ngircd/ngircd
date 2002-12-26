60cf07c8754c9291527fde2791e6353da7674d8c
Alexander Barton <alex@barton.de>
2002-12-26 13:18:46 +0000

    - added strlcpy.c for strlcpy() and strlcat() replacement functions.

---
+++ src/portab/strlcpy.c
@@ -0,0 +1,24 @@
+/*
+ * ngIRCd -- The Next Generation IRC Daemon
+ * Copyright (c)2001,2002 by Alexander Barton (alex@barton.de)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * Please read the file COPYING, README and AUTHORS for more information.
+ *
+ * strlcpy() and strlcat() replacement functions
+ */
+
+
+#include "portab.h"
+
+static char UNUSED id[] = "$Id: strlcpy.c,v 1.1 2002/12/26 13:18:46 alex Exp $";
+
+#include "imp.h"
+
+#include "exp.h"
+
+
+/* -eof- */

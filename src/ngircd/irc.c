129a22a29bb59da08a758b6a29202cc9ef389aef
Alexander Barton <alex@barton.de>
2005-04-18 15:44:39 +0000

    Cleaned up (and documented) the IRC_KILL() function.

--- src/ngircd/irc.c
+++ src/ngircd/irc.c
@@ -14,7 +14,7 @@
 
 #include "portab.h"
 
-static char UNUSED id[] = "$Id: irc.c,v 1.125 2005/03/19 18:43:49 fw Exp $";
+static char UNUSED id[] = "$Id: irc.c,v 1.126 2005/04/18 15:44:39 alex Exp $";
 
 #include "imp.h"
 #include <assert.h>
@@ -54,84 +54,116 @@ IRC_ERROR( CLIENT *Client, REQUEST *Req )
 } /* IRC_ERROR */
 
 
+/**
+ * Kill client on request.
+ * This function implements the IRC command "KILL" wich is used to selectively
+ * disconnect clients. It can be used by IRC operators and servers, for example
+ * to "solve" nick collisions after netsplits.
+ * Please note that this function is also called internally, without a real
+ * KILL command beeing received over the network! Client is Client_ThisServer()
+ * in this case. */
 GLOBAL bool
 IRC_KILL( CLIENT *Client, REQUEST *Req )
 {
 	CLIENT *prefix, *c;
-	char reason[COMMAND_LEN];
+	char reason[COMMAND_LEN], *msg;
 	CONN_ID my_conn, conn;
 
 	assert( Client != NULL );
 	assert( Req != NULL );
 
-	/* Is the user an IRC operator? */
-	if(( Client_Type( Client ) != CLIENT_SERVER ) && ( ! Client_OperByMe( Client ))) return IRC_WriteStrClient( Client, ERR_NOPRIVILEGES_MSG, Client_ID( Client ));
+	if(( Client_Type( Client ) != CLIENT_SERVER ) &&
+	   ( ! Client_OperByMe( Client )))
+	{
+		/* The originator of the KILL is neither an IRC operator of
+		 * this server nor a server. */
+		return IRC_WriteStrClient( Client, ERR_NOPRIVILEGES_MSG,
+					   Client_ID( Client ));
+	}
 
-	/* Bad number of parameters? */
-	if(( Req->argc != 2 )) return IRC_WriteStrClient( Client, ERR_NEEDMOREPARAMS_MSG, Client_ID( Client ), Req->command );
+	if( Req->argc != 2 )
+	{
+		/* This command requires exactly 2 parameters! */
+		return IRC_WriteStrClient( Client, ERR_NEEDMOREPARAMS_MSG,
+					   Client_ID( Client ), Req->command );
+	}
 
 	if( Req->prefix ) prefix = Client_Search( Req->prefix );
 	else prefix = Client;
 	if( ! prefix )
 	{
-		Log( LOG_WARNING, "Got KILL with invalid prefix: \"%s\"!", Req->prefix );
+		Log( LOG_WARNING, "Got KILL with invalid prefix: \"%s\"!",
+		     Req->prefix );
 		prefix = Client_ThisServer( );
 	}
 
-	if( Client != Client_ThisServer( )) Log( LOG_NOTICE|LOG_snotice, "Got KILL command from \"%s\" for \"%s\": %s", Client_Mask( prefix ), Req->argv[0], Req->argv[1] );
+	if( Client != Client_ThisServer( ))
+	{
+		/* This is a "real" KILL received from the network. */
+		Log( LOG_NOTICE|LOG_snotice, "Got KILL command from \"%s\" for \"%s\": %s",
+		     Client_Mask( prefix ), Req->argv[0], Req->argv[1] );
+	}
 
 	/* Build reason string */
-	if( Client_Type( Client ) == CLIENT_USER ) snprintf( reason, sizeof( reason ), "KILLed by %s: %s", Client_ID( Client ), Req->argv[1] );
-	else strlcpy( reason, Req->argv[1], sizeof( reason ));
+	if( Client_Type( Client ) == CLIENT_USER )
+	{
+		/* Prefix the "reason" if the originator is a regular user,
+		 * so users can't spoof KILLs of servers. */
+		snprintf( reason, sizeof( reason ), "KILLed by %s: %s",
+			  Client_ID( Client ), Req->argv[1] );
+	}
+	else
+		strlcpy( reason, Req->argv[1], sizeof( reason ));
 
 	/* Inform other servers */
-	IRC_WriteStrServersPrefix( Client, prefix, "KILL %s :%s", Req->argv[0], reason );
+	IRC_WriteStrServersPrefix( Client, prefix, "KILL %s :%s",
+				   Req->argv[0], reason );
 
 	/* Save ID of this connection */
 	my_conn = Client_Conn( Client );
-	
+
 	/* Do we host such a client? */
 	c = Client_Search( Req->argv[0] );
 	if( c )
 	{
-		/* Yes, there is such a client -- but is it a valid user? */
-		if( Client_Type( c ) == CLIENT_SERVER )
+		if(( Client_Type( c ) != CLIENT_USER ) &&
+		   ( Client_Type( c ) != CLIENT_GOTNICK ))
 		{
-			if( Client != Client_ThisServer( )) IRC_WriteStrClient( Client, ERR_CANTKILLSERVER_MSG, Client_ID( Client ));
-			else
+			/* Target of this KILL is not a regular user, this is
+			 * invalid! So we ignore this case if we received a
+			 * regular KILL from the network and try to kill the
+			 * client/connection anyway (but log an error!) if the
+			 * origin is the local server. */
+
+			if( Client != Client_ThisServer( ))
 			{
-				/* Oops, I should kill another server!? */
-				Log( LOG_ERR, "Can't KILL server \"%s\"!", Req->argv[0] );
-				conn = Client_Conn( Client_NextHop( c ));
-				assert( conn > NONE );
-				Conn_Close( conn, NULL, "Nick collision for server!?", true);
+				/* Invalid KILL received from remote */
+				if( Client_Type( c ) == CLIENT_SERVER )
+					msg = ERR_CANTKILLSERVER_MSG;
+				else
+					msg = ERR_NOPRIVILEGES_MSG;
+				return IRC_WriteStrClient( Client, msg,
+					Client_ID( Client ));
 			}
+
+			Log( LOG_ERR, "Got KILL for invalid client type: %d, \"%s\"!",
+			     Client_Type( c ), Req->argv[0] );
 		}
-		else if( Client_Type( c ) != CLIENT_USER  )
-		{
-			if( Client != Client_ThisServer( )) IRC_WriteStrClient( Client, ERR_NOPRIVILEGES_MSG, Client_ID( Client ));
-			else
-			{
-				/* Oops, what sould I close?? */
-				Log( LOG_ERR, "Can't KILL \"%s\": invalid client type!", Req->argv[0] );
-				conn = Client_Conn( Client_NextHop( c ));
-				assert( conn > NONE );
-				Conn_Close( conn, NULL, "Collision for invalid client type!?", true);
-			}
-		}
-		else
-		{
-			/* Kill user NOW! */
-			conn = Client_Conn( c );
-			Client_Destroy( c, NULL, reason, false );
-			if( conn != NONE ) Conn_Close( conn, NULL, reason, true);
-		}
+
+		/* Kill client NOW! */
+		conn = Client_Conn( Client_NextHop( c ));
+		Client_Destroy( c, NULL, reason, false );
+		if( conn > NONE )
+			Conn_Close( conn, NULL, reason, true );
 	}
-	else Log( LOG_NOTICE, "Client with nick \"%s\" is unknown here.", Req->argv[0] );
+	else
+		Log( LOG_NOTICE, "Client with nick \"%s\" is unknown here.", Req->argv[0] );
 
 	/* Are we still connected or were we killed, too? */
-	if(( my_conn > NONE ) && ( Client_GetFromConn( my_conn ))) return CONNECTED;
-	else return DISCONNECTED;
+	if(( my_conn > NONE ) && ( Client_GetFromConn( my_conn )))
+		return CONNECTED;
+	else
+		return DISCONNECTED;
 } /* IRC_KILL */
 
 

3a2ac66f7ff5985cae4de390a8e510ae7ff2a5a6
Alexander Barton <alex@barton.de>
2010-01-16 14:07:27 +0100

    Added missing modes to USERMODES #define
    Now the numeric 004 correctly reports all the supported user and channel
    modes (user modes "r" and "w" were missing), e. g.:
    
      :a.irc.net 004 a a.irc.net ngircd-15 aiorsw biIklmnoPstv

--- src/ngircd/defines.h
+++ src/ngircd/defines.h
@@ -1,14 +1,12 @@
 /*
  * ngIRCd -- The Next Generation IRC Daemon
- * Copyright (c)2001-2007 Alexander Barton (alex@barton.de)
+ * Copyright (c)2001-2010 Alexander Barton (alex@barton.de)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * Please read the file COPYING, README and AUTHORS for more information.
- *
- * $Id: defines.h,v 1.62 2007/11/21 12:16:36 alex Exp $
  */
 
 
@@ -82,7 +80,7 @@
 #define RECONNECT_DELAY 3		/* Time to delay re-connect attempts
 					   in seconds. */
 
-#define USERMODES "aios"		/* Supported user modes. */
+#define USERMODES "aiorsw"		/* Supported user modes. */
 #define CHANMODES "biIklmnoPstv"	/* Supported channel modes. */
 
 #define CONNECTED true			/* Internal status codes. */

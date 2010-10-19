5f2bc55d36810499a4533589acb45839f4721c84
Alexander Barton <alex@barton.de>
2010-10-19 22:13:12 +0200

    Only use "__attribute__ ((unused))" if GCC >=2.8 is used
    At least GCC 2.7.2 doesn't support this attribute.

--- src/portab/portab.h
+++ src/portab/portab.h
@@ -1,14 +1,12 @@
 /*
  * ngIRCd -- The Next Generation IRC Daemon
- * Copyright (c)2001-2005 Alexander Barton (alex@barton.de)
+ * Copyright (c)2001-2010 Alexander Barton (alex@barton.de)
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  * Please read the file COPYING, README and AUTHORS for more information.
- *
- * $Id: portab.h,v 1.22 2005/07/31 20:13:11 alex Exp $
  *
  * Portability functions and declarations (header for libngbportab).
  */
@@ -48,7 +46,7 @@
 
 /* compiler features */
 
-#if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 7))
+#if (__GNUC__ > 2 || (__GNUC__ == 2 && __GNUC_MINOR__ > 7))
 # define PUNUSED(x) __attribute__ ((unused)) x
 # define UNUSED     __attribute__ ((unused))
 #else

60812b6fdf8ec8b644e1209802cd001a0a032d23
Alexander Barton <alex@barton.de>
2011-11-05 00:35:18 +0100

    defines.h: fix comment: "lenth" -> "length"
    Reported by Christoph Biedl in #ngircd. Thanks!

--- src/ngircd/defines.h
+++ src/ngircd/defines.h
@@ -44,7 +44,7 @@
 					   see RFC 2812, section 1.2.1 */
 #define CLIENT_NAME_LEN 32		/* Max. length of "real names" */
 #define CLIENT_HOST_LEN 64		/* Max. host name length */
-#define CLIENT_MODE_LEN 16		/* Max. lenth of all client modes */
+#define CLIENT_MODE_LEN 16		/* Max. length of all client modes */
 #define CLIENT_INFO_LEN 64		/* Max. length of server info texts */
 #define CLIENT_AWAY_LEN 128		/* Max. length of away messages */
 #define CLIENT_FLAGS_LEN 16		/* Max. length of client flags */

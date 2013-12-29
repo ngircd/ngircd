d913323ca9a0900e836bc4f786850f303c0486ea
Alexander Barton <alex@barton.de>
2013-12-29 17:46:54 +0100

    portabtest: Code cleanup

--- src/portab/portabtest.c
+++ src/portab/portabtest.c
@@ -1,6 +1,6 @@
 /*
  * ngIRCd -- The Next Generation IRC Daemon
- * Copyright (c)2001,2002 by Alexander Barton (alex@barton.de)
+ * Copyright (c)2001-2013 Alexander Barton (alex@barton.de) and Contributors.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,40 +24,45 @@
 
 #include "exp.h"
 
-
 static void Panic PARAMS (( char *Reason, int Code ));
 
-
 GLOBAL int
-main( void )
+main(void)
 {
 	/* validate datatypes */
-	if( false != 0 ) Panic( "false", 1 );
-	if( true != 1 ) Panic( "true", 1 );
-	if( sizeof( UINT8 ) != 1 ) Panic( "UINT8", 1 );
-	if( sizeof( UINT16 ) != 2 ) Panic( "UINT16", 1 );
-	if( sizeof( UINT32 ) != 4 ) Panic( "UINT32", 1 );
+	if (false != 0)
+		Panic("false", 1);
+	if (true != 1)
+		Panic("true", 1);
+	if (sizeof(UINT8) != 1)
+		Panic("UINT8", 1);
+	if (sizeof(UINT16) != 2)
+		Panic("UINT16", 1);
+	if (sizeof(UINT32) != 4)
+		Panic("UINT32", 1);
 
 #ifdef PROTOTYPES
 	/* check functions */
-	if( ! snprintf ) Panic( "snprintf", 2 );
-	if( ! vsnprintf ) Panic( "vsnprintf", 2 );
-	if( ! strlcpy ) Panic( "strlcpy", 2 );
-	if( ! strlcat ) Panic( "strlcat", 2 );
+	if (!snprintf)
+		Panic("snprintf", 2);
+	if (!vsnprintf)
+		Panic("vsnprintf", 2);
+	if (!strlcpy)
+		Panic("strlcpy", 2);
+	if (!strlcat)
+		Panic("strlcat", 2);
 #endif
 	
 	/* ok, no error */
 	return 0;
 } /* portab_check_types */
 
-
 static void
-Panic( char *Reason, int Code )
+Panic(char *Reason, int Code)
 {
 	/* Oops, something failed!? */
-	fprintf( stderr, "Oops, test for %s failed!?", Reason );
-	exit( Code );
+	fprintf(stderr, "Oops, test for %s failed!?", Reason);
+	exit(Code);
 } /* Panic */
-
 
 /* -eof- */
